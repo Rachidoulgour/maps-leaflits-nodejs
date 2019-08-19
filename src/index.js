0b2fc33c61304b4bfdbcb788ac135e400f3db2fb
rachid <olgor@hotmail.com>
2019-08-19 19:07:42 +0200

    routes added

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const engine = require('ejs-mate');
+const path = require('path');
 //initializations
 const app = express();
 
 // setings
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 // routes
-app.get('/', (req,res)=>{
-    res.render('index');
-});
+app.use(require('./routes/'));
 
 app.listen(3000, ()=>{
     console.log('Server on port 3000');
